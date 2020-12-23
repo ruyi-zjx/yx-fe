7f8c7c71aa4b1f0ed1c47421e5ac1b6ad4305da3
zjx <ruyi_zozo@163.com>
2020-12-23 16:24:47 +0800

    fix：修复VVisible

--- packages/helper/src/directives/visible.ts
+++ packages/helper/src/directives/visible.ts
@@ -1,57 +1,55 @@
-import { VNode } from 'vue';
-import { DirectiveBinding } from 'vue/types/options';
-import { IVVisibleYx } from '../../types/visible';
-import getPageSetting from '../get-page-setting';
+import { VNode } from 'vue'
+import { DirectiveBinding } from 'vue/types/options'
+import { IVVisibleYx } from '../../types/visible'
+import getPageSetting from '../get-page-setting'
 
-const DIRE = `[ visible-yx ] `;
+const DIRE = `[ visible-yx ] `
 
 const VVisible: IVVisibleYx = {
   // 动态设置
   dynamicSetting: undefined,
 
   install: function (Vue, options) {
-    if (!options?.store) return console.warn(DIRE + '注册指令必须传入`vuex`实例，注册失败');
-    const dynamicSetting = options.store[options.dynamicStoreKey || 'dynamicSetting'];
-    if (!dynamicSetting) return console.warn(DIRE + '未发现存储动态配置的`module`，注册失败');
-    this.dynamicSetting = dynamicSetting;
+    if (!options?.store) return console.error(DIRE + '指令注册失败：注册指令必须传入`vuex`实例')
+    const dynamicSetting = options.store.state[options.dynamicStoreKey || 'dynamicSetting']
+    if (!dynamicSetting) return console.error(DIRE + '指令注册失败：未发现存储动态配置的`module`.')
+    this.dynamicSetting = dynamicSetting
     Vue.directive('visible-yx', {
       inserted: this.inserted.bind(this),
-    });
+    })
   },
 
   inserted: function (el, binding, vnode) {
     // 页面元素名
-    const elementName = binding.arg;
-    if (!elementName) return;
+    const elementName = binding.arg
+    if (!elementName) return
     // 当前页面的路由
-    const route = vnode.context.$route;
+    const route = vnode.context.$route
     // 当前页面数据
-    const setting = getPageSetting(route, this.dynamicSetting?.pages);
+    const setting = getPageSetting(route, this.dynamicSetting?.pages)
     // 当前页面无配置？直接退出
-    if (!setting) return;
+    if (!setting) return
     // 从修饰符中获取当前组件类型
-    const component = Object.keys(binding.modifiers)[0] || 'default';
+    const component = Object.keys(binding.modifiers)[0] || 'default'
     // 预置好的组件处理函数
     const presetCompHandler = {
       table: handleTable,
-    };
+    }
     if (component === 'default') {
-      handleDefault(setting, elementName, el, binding, vnode);
+      handleDefault(setting, elementName, el, binding, vnode)
     } else if (Object.keys(presetCompHandler).includes(component)) {
-      const compSetting = setting[component];
-      if (!compSetting) return;
+      const compSetting = setting[component]
+      if (!compSetting) return
       // 因为一个页面可能有多个相同的组件，所以这边继续拿指定元素名的组件配置
-      const eleSetting = compSetting[elementName];
-      if (!eleSetting || !Object.keys(eleSetting).length) return;
-      const handler = setting[component];
-      handler(eleSetting, elementName, el, binding, vnode);
+      const eleSetting = compSetting[elementName]
+      if (!eleSetting || !Object.keys(eleSetting).length) return
+      const handler = setting[component]
+      handler(eleSetting, elementName, el, binding, vnode)
     } else {
-      console.warn(
-        `${DIRE} 未发现\`${component}\`组件类型的处理函数，请检查\`${binding.rawName}\`指令语法是否正确`,
-      );
+      console.warn(`${DIRE} 未发现\`${component}\`组件类型的处理函数，请检查\`${binding.rawName}\`指令语法是否正确`)
     }
   },
-};
+}
 
 // 处理一般的元素，单纯控制元素是否显示或者隐藏
 function handleDefault(
@@ -59,11 +57,11 @@ function handleDefault(
   elementName: string,
   el: HTMLElement,
   binding: DirectiveBinding,
-  vnode: VNode,
+  vnode: VNode
 ) {
-  const { visible = [] } = setting;
+  const { visible = [] } = setting
   if (!visible.includes(elementName)) {
-    (el.parentNode && el.parentNode.removeChild(el)) || (el.style.display = 'none');
+    ;(el.parentNode && el.parentNode.removeChild(el)) || (el.style.display = 'none')
   }
 }
 
@@ -72,25 +70,25 @@ function handleTable(
   elementName: string,
   el: HTMLElement,
   binding: DirectiveBinding,
-  vnode: VNode,
+  vnode: VNode
 ) {
-  const vm = vnode.componentInstance;
-  const { columns = [], scroll = {}, visible = true } = setting;
+  const vm = vnode.componentInstance
+  const { columns = [], scroll = {}, visible = true } = setting
   if (!visible) {
-    (el.parentNode && el.parentNode.removeChild(el)) || (el.style.display = 'none');
+    ;(el.parentNode && el.parentNode.removeChild(el)) || (el.style.display = 'none')
   }
   // 1. 处理需要显示的列
   if (columns.length) {
-    const fullColumns = vm?.columns || [];
+    const fullColumns = vm?.columns || []
     for (let i = fullColumns.length - 1; i >= 0; i--) {
-      const target = fullColumns[i];
+      const target = fullColumns[i]
       if (!columns.includes(target.dataIndex || target.key)) {
-        fullColumns.splice(i, 1);
+        fullColumns.splice(i, 1)
       }
     }
   }
   // 2. 处理横向或纵向滚动距离
-  Object.assign(vm?.scroll, scroll);
+  Object.assign(vm?.scroll, scroll)
 }
 
-export default VVisible;
+export default VVisible
--- packages/helper/src/get-page-setting.ts
+++ packages/helper/src/get-page-setting.ts
@@ -1,25 +1,22 @@
-import { Route } from 'vue-router';
-const { pathToRegexp } = require('path-to-regexp');
+import { Route } from 'vue-router'
+const { pathToRegexp } = require('path-to-regexp')
 
 /**
  * 获取目标页面的动态配置
  * @param {*} route 页面的路由
  * @param {*} pages 所有的页面配置
  */
-export default function getPageSetting(
-  route: Route,
-  pages?: DynamicSetting['pages'],
-): PageSetting | undefined {
-  let curSetting;
-  if (!pages) return curSetting;
+export default function getPageSetting(route: Route, pages?: DynamicSetting['pages']): PageSetting | undefined {
+  let curSetting
+  if (!pages) return curSetting
   for (let i = 0, len = pages.length; i < len; i++) {
-    const cur = pages[i];
-    const regex = pathToRegexp(cur.path);
+    const cur = pages[i]
+    const regex = pathToRegexp(cur.path)
     // 使用正则进行匹配，找出该页面的相关配置
     if (regex.exec(route.path)) {
-      curSetting = cur;
-      break;
+      curSetting = cur
+      break
     }
   }
-  return curSetting;
+  return curSetting
 }
